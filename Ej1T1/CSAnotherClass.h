314b74a0166ab427270df3d2194b2f78ac24e913
carlos <carlos.sguzman@gmail.com>
2014-04-09 21:03:21 +0200

    New class

---
+++ Ej1T1/CSAnotherClass.h
@@ -0,0 +1,13 @@
+//
+//  CSAnotherClass.h
+//  Ej1T1
+//
+//  Created by Carlos Salguero Guzmán on 09/04/14.
+//  Copyright (c) 2014 Carlos Salguero Guzmán. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CSAnotherClass : NSObject
+
+@end
---
+++ Ej1T1/CSAnotherClass.m
@@ -0,0 +1,13 @@
+//
+//  CSAnotherClass.m
+//  Ej1T1
+//
+//  Created by Carlos Salguero Guzmán on 09/04/14.
+//  Copyright (c) 2014 Carlos Salguero Guzmán. All rights reserved.
+//
+
+#import "CSAnotherClass.h"
+
+@implementation CSAnotherClass
+
+@end
